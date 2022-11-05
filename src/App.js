823b70d1010406a69e51848306f84eb62491f61b
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-11-05 11:46:09 +0000

    feat: adding hooks and http GET to https://jsonplaceholder.typicode.com/posts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage,  setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(()=>{
+    const fetchPosts = async () =>{
+      setLoading(true);
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(response.data);
+      setLoading(false);
+    }
+
+    fetchPosts();
+  },[]);
+
+  console.log(posts);
   return (
-    <div>
+    <div className="container">
       <h2>Let's get started!</h2>
     </div>
   );
