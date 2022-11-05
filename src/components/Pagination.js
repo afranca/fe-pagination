f4ad74a47cfa47c3b66962e8ffddbd72d7511663
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-11-05 18:46:17 +0000

    feat: Pagination Controls working perfectly

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -7,20 +7,35 @@ const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage }) => {
   const previousPage = currentPage-1;
 
   const changePageHandler = (numb) =>{
-    //console.log(numb);
+    // This makes sure the controls don't point
+    // at pages ouf of boundaries 
     if (numb < 1){
         numb = 1;
-    }
-    if (numb > lastPageNumber){
+    } else if (numb > lastPageNumber){
         numb = lastPageNumber;
     }
-    //console.log(numb);
+    
     paginate(numb);
   };  
 
-  const pageNumbers = [];      
+  const pageNumbers = []; 
 
-  for (let i =1; i<=lastPageNumber; i++){
+  // This decides the interval of pages currently displayed
+  // in the pagination controls
+  let lastPageNumberOnDisplay;
+  let firstPageNumberOnDisplay;
+  if (currentPage <= 3){
+    firstPageNumberOnDisplay = 1;
+    lastPageNumberOnDisplay = 5;
+  } else if (currentPage >= lastPageNumber - 3){
+    firstPageNumberOnDisplay = lastPageNumber - 4;
+    lastPageNumberOnDisplay = lastPageNumber;
+  } else {
+    firstPageNumberOnDisplay = currentPage - 2;
+    lastPageNumberOnDisplay = currentPage + 2;
+  }
+
+  for (let i = firstPageNumberOnDisplay; i<=lastPageNumberOnDisplay; i++){
     pageNumbers.push(i);
   }
   return (
@@ -51,7 +66,7 @@ const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage }) => {
             </a>              
         </li>
         <li key={Math.random()} className="page-item">
-            <a href='!#' className='page-link' onClick={ () => changePageHandler(Math.ceil(totalPosts/postsPerPage)) }>
+            <a href='!#' className='page-link' onClick={ () => changePageHandler(lastPageNumber) }>
                 »
             </a>        
         </li>            
